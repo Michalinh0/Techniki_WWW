21bc9c0195fa02d26d9c41e87bc537a963230b2a
Michał Machowski <machowski.michal01@gmail.com>
2023-01-24 16:04:05 +0100

    Some index loading

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const app = express();
 const port = 3000
 app.set("view engine" , "ejs")
 
+app.use(express.static(__dirname + '/public'));
+
 const indexRouter = require('./routes/index')
 
 app.use('/' , indexRouter)
