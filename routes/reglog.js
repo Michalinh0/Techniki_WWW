cc5a7783df42c5bac4bf7de4fc24b064bd1602d1
Michał Machowski <machowski.michal01@gmail.com>
2023-02-05 23:36:07 +0100

    Reg done - todo - collection - exchange - panel

--- routes/reglog.js
+++ routes/reglog.js
@@ -9,7 +9,39 @@ router.get('/' , (req , res) =>{
 });
 
 router.post('/reg' , (req , res) =>{
-    res.redirect('/log')
+  var mail = req.body.Email
+  var login = req.body.Login
+  var password = req.body.Password
+  var password2 = req.body.PasswordRepeat
+
+  if (password !== password2) {
+    return res.redirect('/reglog?error=passwords-do-not-match');
+    }
+
+  let checkLoginAndEmailSQL = `SELECT COUNT(*) as count FROM Uzytkownicy WHERE email = ? OR login = ?`;
+    req.db.get(checkLoginAndEmailSQL, [mail, login], (err, row) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      if (row.count > 0) {
+        // Email or login already exists, redirect back to the register page with an error message
+        return res.redirect('/reglog?error=registration-failed');
+      }
+    });
+
+  let isql = 'Insert into Uzytkownicy Values (? , ? , ? , 1)';
+  req.db.run(isql, [mail, login, password], function(err) {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    req.session.user = {
+      mail: mail
+    };
+    res.redirect('/log');
+  });
+
 });
 
 router.post('/log' , (req , res) =>{
